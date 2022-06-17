18fbaa24fe44f600f658526f1e7c503812763ea8
unalo <unalomtshokotsha@gmail.com>
2022-06-18 00:45:20 +0200

    added mongoose set-up, app skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import config from  'config';
 import connect from './utils/connect';
-import logger from './utils/logger';
+// import logger from './utils/logger';
 import routes from './routes'
 
 const port = config.get<number>('port');
